08f0ef5ab2d897f2406dfe17f69d71723cb9e750
Peter Liu <onhout@gmail.com>
2018-06-16 18:36:52 -0700

    formatted

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -8,7 +8,7 @@ class MainController < ApplicationController
   end
 
   def reports
-    send_data(generate_pdf, :filename => "report.pdf", :type => "application/pdf")
+    send_data(generate_pdf, filename: "report.pdf", type: "application/pdf")
   end
 
   def generate_pdf
@@ -19,36 +19,36 @@ class MainController < ApplicationController
       gainer = JSON.parse(gainer_res)
       loser = JSON.parse(loser_res)
 
-      text "<font size='24'> Top Gainers </font>", :inline_format => true
+      text "<font size='24'> Top Gainers </font>", inline_format: true
       gainer.each() do |list|
         float do
-          bounding_box [15, cursor], :width => 10 do
+          bounding_box [15, cursor], width: 10 do
             text "*"
           end
         end
 
-        bounding_box [25, cursor], :width => 600 do
+        bounding_box [25, cursor], width: 600 do
           text "#{list["symbol"]} - #{list["companyName"]}"
           text "Exchange: #{list["primaryExchange"]} | Sector: #{list["sector"]}"
           text "Open: $#{list["open"]}, High: $#{list["high"]} , Low: $#{list["low"]} , Close: $#{list["close"]}"
-          text "PERCENT CHANGE: <color rgb='006400'> #{(list["changePercent"] * 100).round(3)}% </color>", :inline_format => true
+          text "PERCENT CHANGE: <color rgb='006400'> #{(list["changePercent"] * 100).round(3)}% </color>", inline_format: true
         end
         move_down(5)
       end
       start_new_page
-      text "<font size='24'> Top Losers </font>", :inline_format => true
+      text "<font size='24'> Top Losers </font>", inline_format: true
       loser.each() do |list|
         float do
-          bounding_box [15, cursor], :width => 10 do
+          bounding_box [15, cursor], width: 10 do
             text "*"
           end
         end
 
-        bounding_box [25, cursor], :width => 600 do
+        bounding_box [25, cursor], width: 600 do
           text "#{list["symbol"]} - #{list["companyName"]}"
           text "Exchange: #{list["primaryExchange"]} | Sector: #{list["sector"]}"
           text "Open: $#{list["open"]}, High: $#{list["high"]} , Low: $#{list["low"]} , Close: $#{list["close"]}"
-          text "PERCENT CHANGE: <color rgb='8B0000'> #{(list["changePercent"] * 100).round(3)}% </color>", :inline_format => true
+          text "PERCENT CHANGE: <color rgb='8B0000'> #{(list["changePercent"] * 100).round(3)}% </color>", inline_format: true
         end
         move_down(5)
       end
