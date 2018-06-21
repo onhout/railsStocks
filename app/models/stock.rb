7bac8fa1569ee71381743189318026083ca316f9
Peter Liu <onhout@gmail.com>
2018-06-21 10:28:56 -0700

    just testing one more theory

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,7 +2,8 @@ class Stock < ApplicationRecord
   validates_presence_of :symbol, :name, :symbol_type
 
   def self.get_set(stock)
-    st = Stock.find_or_initialize_by(symbol: stock["symbol"])
+    st = find_or_initialize_by(symbol: stock["symbol"])
+    # already defined self thereby no need for Stock/self.
     st.name = stock["name"]
     st.symbol_type = stock["type"]
     st.save
