154287a6f8b8fa65bbe37caf0f832000587d8f99
Peter Liu <onhout@gmail.com>
2018-06-21 10:26:00 -0700

    fat model skinny controller examples

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -59,10 +59,7 @@ class MainController < ApplicationController
     stocks_symbol_url = URI.parse('https://api.iextrading.com/1.0/ref-data/symbols')
     stock_hash = JSON.parse Net::HTTP.get(stocks_symbol_url)
     stock_hash.each do |stock|
-      st = Stock.find_or_initialize_by(symbol: stock["symbol"])
-      st.name = stock["name"]
-      st.symbol_type = stock["type"]
-      st.save
+      Stock.get_set(stock)
     end
     json_response({message: "Success"})
   end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,3 +1,10 @@
 class Stock < ApplicationRecord
   validates_presence_of :symbol, :name, :symbol_type
+
+  def self.get_set(stock)
+    st = Stock.find_or_initialize_by(symbol: stock["symbol"])
+    st.name = stock["name"]
+    st.symbol_type = stock["type"]
+    st.save
+  end
 end
